02b17c9355965381a666e3cfbc362cd5f65bb51c
Nicolas Hoffmann <dante3333@gmail.com>
2017-12-01 13:41:24 +0100

    Fixed in existing aria-describedby attribute
    Closed #11

--- jquery-accessible-simple-tooltip-aria.js
+++ jquery-accessible-simple-tooltip-aria.js
@@ -4,7 +4,7 @@
 
     /*
      * jQuery accessible simple (non-modal) tooltip window, using ARIA
-     * @version v2.0.4
+     * @version v2.2.0 
      * Website: https://a11y.nicolas-hoffmann.net/simple-tooltip/
      * License MIT: https://github.com/nico3333fr/jquery-accessible-simple-tooltip-aria/blob/master/LICENSE
      */
@@ -17,9 +17,10 @@
         var content_id = typeof options.simpletooltipContentId !== 'undefined' ? '#' + options.simpletooltipContentId : '';
 
         var index_lisible = Math.random().toString(32).slice(2, 12);
+        var aria_describedby = element.attr('aria-describedby') || '';
 
         element.attr({
-            'aria-describedby': 'label_simpletooltip_' + index_lisible
+            'aria-describedby': 'label_simpletooltip_' + index_lisible + ' ' + aria_describedby
         });
 
         element.wrap('<span class="' + prefix_class + 'simpletooltip_container"></span>');
@@ -54,19 +55,43 @@
         $('body')
             .on('mouseenter focusin', '.js-simple-tooltip', function() {
                 var $this = $(this);
-                var $tooltip_to_show = $('#' + $this.attr('aria-describedby'));
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby.substr(0, aria_describedby.indexOf(" "));
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
                 $tooltip_to_show.attr('aria-hidden', 'false');
             })
-            .on('mouseleave focusout', '.js-simple-tooltip', function() {
+            .on('mouseleave', '.js-simple-tooltip', function(event) {
                 var $this = $(this);
-                var $tooltip_to_show = $('#' + $this.attr('aria-describedby'));
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby.substr(0, aria_describedby.indexOf(" "));
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
+                var $is_target_hovered = $tooltip_to_show.is(':hover');
+
+                //alert($target_hovered);
+                //$target.addClass('redborder');
+                if (!$is_target_hovered) {
+                    $tooltip_to_show.attr('aria-hidden', 'true');
+                }
+            })
+            .on('focusout', '.js-simple-tooltip', function(event) {
+                var $this = $(this);
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby.substr(0, aria_describedby.indexOf(" "));
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
+
                 $tooltip_to_show.attr('aria-hidden', 'true');
+            })
+            .on('mouseleave', '.js-simpletooltip', function() {
+                var $this = $(this);
+                $this.attr('aria-hidden', 'true');
             })
             .on('keydown', '.js-simple-tooltip', function(event) {
                 // close esc key
 
                 var $this = $(this);
-                var $tooltip_to_show = $('#' + $this.attr('aria-describedby'));
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby.substr(0, aria_describedby.indexOf(" "));
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
 
                 if (event.keyCode == 27) { // esc
                     $tooltip_to_show.attr('aria-hidden', 'true');
